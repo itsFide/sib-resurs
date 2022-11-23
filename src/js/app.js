5e8061e5cf0d9f01eeaf63ec3983160230148b7e
Sergey <murzak301@gmail.com>
2022-11-23 10:53:45 +0600

    finish first step

--- src/js/app.js
+++ src/js/app.js
@@ -16,3 +16,32 @@ isWebp()
 // Фиксированный header ====================================================================================================================================================
 // headerFixed()
 // ====================================================================================================================================================
+
+// burger menu
+window.onload = ( ) =>{
+    let burger = document.querySelector('.burger');
+    let menu = document.querySelector('.header__nav-mobile')
+    let menuDropdown = menu.querySelectorAll('.has-children')
+    burger.addEventListener('click',()=>{
+        burger.classList.toggle('active')
+       
+            menu.classList.toggle('active')
+            // menu.style.height = `${menu.scrollHeight}px`
+            // console.log(menuDropdown);
+            menuDropdown.forEach((e)=>{
+                // e.classList.add('no-hover')
+                // console.log(e.querySelector('a'));
+                let dropDownLink = e.querySelector('a');
+                dropDownLink.addEventListener('click',(event)=>{
+                    event.preventDefault()
+                    let dropDownItem = e.querySelector('.submenu')
+                    // dropDownItem.style.display = `flex`
+                    dropDownItem.classList.toggle('active')
+                    // dropDownItem.style.height = `${dropDownItem.scrollHeight}px`
+                    // menu.style.height = `${menu.scrollHeight +dropDownItem.scrollHeight / 2 }px`
+                })
+            })
+        
+    })
+}
+    
